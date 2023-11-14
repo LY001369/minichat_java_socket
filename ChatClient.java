2ac00aa1700f953c0ced0e2671f03e6b0ac26f7d
Thạch Tấn Lộc <ly001369@gmail.com>
2023-11-14 08:09:57 +0700

    Upload java file

---
+++ ChatClient.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.net.*;
+
+public class ChatClient {
+    public static void main(String[] args) {
+        try {
+
+    //=====================================================
+            String ServerIP = "localhost";
+            int ServerPort = 1311;
+    //=====================================================
+
+            // Tạo một Kết nối đến Server
+            Socket socket = new Socket(ServerIP, ServerPort);
+
+            BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+            System.out.print("Enter your username: ");
+            String username = consoleReader.readLine();
+            out.println(username);
+
+            new Thread(() -> {
+                try {
+                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    String serverResponse;
+                    while ((serverResponse = in.readLine()) != null) {
+                        System.out.println(serverResponse);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+
+            String message;
+            while (true) {
+                message = consoleReader.readLine();
+                out.println(message);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
